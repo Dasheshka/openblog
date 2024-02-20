f2085fa20ec4696b4f09eb20c3051b8928155e5c
Vlad Gerasimovich <blueambr@icloud.com>
2024-02-20 21:59:04 +0100

    feat: update fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   darkMode: "class",
@@ -11,5 +12,60 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    plugin(({ addBase }) => {
+      addBase({
+        "@font-face": {
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: 200,
+          display: "swap",
+          src: 'url("/fonts/Manrope-ExtraLight.woff2") format("woff2")',
+        },
+        "@font-face": {
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: 300,
+          display: "swap",
+          src: 'url("/fonts/Manrope-Light.woff2") format("woff2")',
+        },
+        "@font-face": {
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: 400,
+          display: "swap",
+          src: 'url("/fonts/Manrope-Regular.woff2") format("woff2")',
+        },
+        "@font-face": {
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: 500,
+          display: "swap",
+          src: 'url("/fonts/Manrope-Medium.woff2") format("woff2")',
+        },
+        "@font-face": {
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: 600,
+          display: "swap",
+          src: 'url("/fonts/Manrope-SemiBold.woff2") format("woff2")',
+        },
+        "@font-face": {
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: 700,
+          display: "swap",
+          src: 'url("/fonts/Manrope-Bold.woff2") format("woff2")',
+        },
+        "@font-face": {
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: 800,
+          display: "swap",
+          src: 'url("/fonts/Manrope-ExtraBold.woff2") format("woff2")',
+        },
+      });
+    }),
+    require("@tailwindcss/typography"),
+  ],
 };
