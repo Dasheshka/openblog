bf9034bce95c510a855875ad4c3b636b55142403
Vlad Gerasimovich <blueambr@icloud.com>
2024-02-21 19:05:08 +0100

    feat: update Header, add aria-label

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdoc,mdx}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       fontFamily: {
         body: ["Manrope", ...defaultTheme.fontFamily.sans],
